3581cc06cf46a936bb5cd62676375d9c45d681c4
pigorsasha <iams1829@gmail.com>
2019-03-06 23:27:48 +0300

    DEV-1 v.1

---
+++ DEV-1/DEV-1/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DEV_1
+{   /// <summary>
+    /// Program which realized method SearchMethod.
+    /// /// <param name="args">Arguments from command line</param>
+    /// </summary>
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            if (args[0].Length < 2)
+            {
+                throw new ArgumentException("Cannot be less than 2!");
+            }
+            SubstringSeach.SearchMethod(args[0]);
+        }
+    }
+}
---
+++ DEV-1/DEV-1/SubstringSeach.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DEV_1
+{   /// <summary>
+    /// In the class there is a method realized which displays all the substrings(two and more symbols),
+    /// where there are no any two subsequent repeated symbols.
+    /// </summary>
+    public class SubstringSeach
+    {
+        public static void SearchMethod(string substring)
+        {
+            List<string> ListOfSubstrings = new List<string>();
+            for (int i = 0; i < substring.Length - 1; i++)
+            {
+                string mainString = substring[i].ToString();
+                for (int j = i; j < substring.Length - 1; j++)
+                {
+                    if (substring[j] != substring[j + 1])
+                    {
+                        mainString += substring[j + 1];
+                    }
+                    else
+                    {
+                        mainString = null;
+                        break;
+                    }
+                    if (!ListOfSubstrings.Contains(mainString) && mainString.Length > 1)
+                    {
+                        ListOfSubstrings.Add(mainString);
+                    }
+                }
+            }
+            foreach (var elements in ListOfSubstrings)
+            {
+                Console.WriteLine(elements);
+            }
+        }
+    }
+}
