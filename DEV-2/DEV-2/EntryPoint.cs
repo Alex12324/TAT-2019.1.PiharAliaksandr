17aa851a5e37606ff594b6b1f03b950727d806f0
pigorsasha <iams1829@gmail.com>
2019-03-15 19:27:41 +0300

    first commit

---
+++ DEV-2/DEV-2/EntryPoint.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace DEV_2
+{   
+    /// <summary>
+    /// My entry point.
+    /// </summary>
+    class EntryPoint
+    {
+        /// <summary>
+        /// My entry point function.
+        /// </summary>
+        /// <param name="args">Arguments from command line</param>
+        static void Main(string[] args)
+        {
+            try
+            {
+                PhoneticConverter elem = new PhoneticConverter(args[0]);
+                elem.Converter();
+                elem.Display();
+                
+            }
+            catch (IndexOutOfRangeException)
+            {
+                Console.WriteLine("Error! String is empty!");
+            }
+            catch(Exception ex)
+            {
+                Console.WriteLine("Something wrong!" + ex.Message);
+            }
+        }
+    }
+}
