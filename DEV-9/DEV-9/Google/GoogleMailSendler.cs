2a24169429dee60a07d449a73bbebfe246975410
pigorsasha <iams1829@gmail.com>
2019-05-10 16:31:26 +0300

    first commit

---
+++ DEV-9/DEV-9/Google/GoogleMailSendler.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Support.UI;
+
+namespace DEV_9.Google
+{
+    class GoogleMailSendler
+    {
+        private IWebDriver _driver { get; set; }
+        private WebDriverWait _wait { get; }
+
+        public IWebElement NewLetter => _wait.Until(t => this._driver.FindElement(By.XPath("//div[@class = 'T-I J-J5-Ji T-I-KE L3']")));
+
+        public IWebElement Recipient => _wait.Until(t => this._driver.FindElement(By.XPath("//textarea[@name = 'to']")));
+
+        public IWebElement TextOfMessage => _wait.Until(t => this._driver.FindElement(By.XPath("//div[@role='textbox']")));
+
+        public IWebElement SubmitButton => _wait.Until(t => this._driver.FindElement(By.XPath("//div[@class='dC']")));
+
+        public GoogleMailSendler(IWebDriver driver)
+        {
+            _driver = driver;
+            _wait = new WebDriverWait(_driver, TimeSpan.FromSeconds(30));
+        }
+
+        public void SendLetter(string recipient,string text)
+        {
+            this.NewLetter.Click();
+            this.Recipient.SendKeys(recipient);
+            this.TextOfMessage.SendKeys(text);
+            this.SubmitButton.Click();
+        }
+
+        public GoogleMailReadPage ChangeAlias()
+        {
+            return new GoogleMailReadPage(this._driver);
+        }
+    }
+}
---
+++ DEV-9/DEV-9/Yandex/YandexMainPage.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Support.UI;
+
+namespace DEV_9.Yandex
+{
+    public class YandexMainPage
+    {
+        private IWebDriver _driver { get; set; }
+        private WebDriverWait _wait { get; }
+
+        public IWebElement LoginButton => _wait.Until(t => this._driver.FindElement(By.XPath("//a[contains(@class,'button desk-notif-card__login-enter-expanded')]")));
+
+        public IWebElement Username => _wait.Until(t => this._driver.FindElement(By.XPath("//input[@name='login']")));
+
+        public IWebElement Password => _wait.Until(t => this._driver.FindElement(By.XPath("//input[@name='passwd']")));
+
+        public IWebElement SubmitButton => _wait.Until(t => this._driver.FindElement(By.XPath("//button[@type='submit']")));
+
+        public IWebElement ErrorMessage => _wait.Until(t => this._driver.FindElement(By.XPath("//div[@class='passp-form-field__error']")));
+
+        public YandexMainPage(IWebDriver driver)
+        {
+            _driver = driver;
+            _wait = new WebDriverWait(_driver, TimeSpan.FromSeconds(30));
+        }
+
+        public void GoToURL()
+        {
+            _driver.Navigate().GoToUrl("https://yandex.by/");
+        }
+
+        public YandexMailSender Login(string username,string password)
+        {
+            this.LoginButton.Click();
+            this.Username.SendKeys(username);
+            this.SubmitButton.Click();
+            this.Password.SendKeys(password);
+            this.SubmitButton.Click();
+            return new YandexMailSender(_driver);
+        }
+
+        public YandexMainPage LoginError(string username, string password)
+        {
+            this.LoginButton.Click();
+            this.Username.SendKeys(username);
+            this.LoginButton.Click();
+            if (this._driver.FindElements(By.XPath("//div[@class='passp-form-field__error']")).Count == 0)
+            {
+                this.Password.SendKeys(password);
+                this.LoginButton.Click();
+            }
+
+            return this;
+        }
+    }
+}
